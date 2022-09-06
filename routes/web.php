6e4a9e0a98940c625fd27b343bf5f57fe9313ece
takuma matsunaga <matsu@takumanoMacBook-Pro.local>
2022-09-06 20:32:57 +0900

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TweetController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::resource('tweet', TweetController::class);
 
 Route::get('/', function () {
     return view('welcome');
