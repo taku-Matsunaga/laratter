0b1ab60de95354a8885e9f81193bd46a1f087865
taku-Matsunaga <skyline199306@yahoo.co.jp>
2022-09-20 20:20:27 +0900

    search

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\FollowController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\TweetController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +20,19 @@ use Illuminate\Support\Facades\Route;
 Route::group(
     ['middleware' => 'auth'],
     function () {
+
+
+        Route::get('/tweet/search/input', [SearchController::class, 'create'])->name('search.input');
+        Route::get('/tweet/search/result', [SearchController::class, 'index'])->name('search.result');
+
+        Route::get('user/{user}', [FollowController::class, 'show'])->name('follow.show');
+        Route::post('user/{user}/follow', [FollowController::class, 'store'])->name('follow');
+        Route::post('user/{user}/unfollow', [FollowController::class, 'destroy'])->name('unfollow');
+
+        Route::post('tweet/{tweet}/favorites', [FavoriteController::class, 'store'])->name('favorites');
+        Route::post('tweet/{tweet}/unfavorites', [FavoriteController::class, 'destroy'])->name('unfavorites');
+        Route::get('/tweet/timeline', [TweetController::class, 'timeline'])->name('tweet.timeline');
+
         Route::get('/tweet/mypage', [TweetController::class, 'mydata'])->name('tweet.mypage');
         Route::resource('tweet', TweetController::class);
     }
